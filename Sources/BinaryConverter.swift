8c9ed6ebab96b17903bee97f4bcacc90c5cca7c2
OTAKE Takayoshi <takayoshiotake@users.noreply.github.com>
2019-03-02 22:19:17 +0900

    Rename convert => parse or binarize

--- Sources/BinaryConverter.swift
+++ Sources/BinaryConverter.swift
@@ -59,13 +59,13 @@ public class BinaryConverter {
     // MARK: - Converting `[UInt8]` into `BinaryCompatible` value(s)
     
     /// Converts the `[UInt8]` into `T`, by reading the `binary`.
-    public class func convert<T: BinaryCompatible>(binary array: Array<UInt8>, byteOrder: ByteOrder? = nil) throws -> T {
-        return try convert(binary: ReadableByteStreamRefering(array[0..<array.count]), byteOrder: byteOrder)
+    public class func parse<T: BinaryCompatible>(binary array: Array<UInt8>, byteOrder: ByteOrder? = nil) throws -> T {
+        return try parse(binary: ReadableByteStreamRefering(array[0..<array.count]), byteOrder: byteOrder)
     }
     
     /// Converts the `[UInt8]` into `T`, by reading the `binary`.
-    public class func convert<T: BinaryCompatible>(binary arraySlice: ArraySlice<UInt8>, byteOrder: ByteOrder? = nil) throws -> T {
-        return try convert(binary: ReadableByteStreamRefering(arraySlice), byteOrder: byteOrder)
+    public class func parse<T: BinaryCompatible>(binary arraySlice: ArraySlice<UInt8>, byteOrder: ByteOrder? = nil) throws -> T {
+        return try parse(binary: ReadableByteStreamRefering(arraySlice), byteOrder: byteOrder)
     }
 
     /// Converts the `[UInt8]` into `T`, by reading the `binary`.
@@ -74,19 +74,19 @@ public class BinaryConverter {
     /// - parameter byteOrder: the byte order of the converting value; default is `nil` (optional)
     /// - returns: the value converted
     /// - throws: BinaryConverterError.streamIsShort: Could not read the value from the binary
-    public class func convert<T: BinaryCompatible>(binary stream: ReadableByteStream, byteOrder: ByteOrder? = nil) throws -> T {
+    public class func parse<T: BinaryCompatible>(binary stream: ReadableByteStream, byteOrder: ByteOrder? = nil) throws -> T {
         return try T(parsing: stream, byteOrder: byteOrder)
     }
     
     
     /// Converts the `[UInt8]` into `[T]`, by reading the `binary`.
-    public class func convert<T: BinaryCompatible>(binary array: Array<UInt8>, count: Int, byteOrder: ByteOrder? = nil) throws -> [T] {
-        return try convert(binary: ReadableByteStreamRefering(array[0..<array.count]), count: count, byteOrder: byteOrder)
+    public class func parse<T: BinaryCompatible>(binary array: Array<UInt8>, count: Int, byteOrder: ByteOrder? = nil) throws -> [T] {
+        return try parse(binary: ReadableByteStreamRefering(array[0..<array.count]), count: count, byteOrder: byteOrder)
     }
     
     /// Converts the `[UInt8]` into `[T]`, by reading the `binary`.
-    public class func convert<T: BinaryCompatible>(binary arraySlice: ArraySlice<UInt8>, count: Int, byteOrder: ByteOrder? = nil) throws -> [T] {
-        return try convert(binary: ReadableByteStreamRefering(arraySlice), count: count, byteOrder: byteOrder)
+    public class func parse<T: BinaryCompatible>(binary arraySlice: ArraySlice<UInt8>, count: Int, byteOrder: ByteOrder? = nil) throws -> [T] {
+        return try parse(binary: ReadableByteStreamRefering(arraySlice), count: count, byteOrder: byteOrder)
     }
     
     /// Converts the `[UInt8]` into `[T]`, by reading the `binary`.
@@ -96,7 +96,7 @@ public class BinaryConverter {
     /// - parameter byteOrder: the byte order of the converting value; default is `nil` (optional)
     /// - returns: the value converted
     /// - throws: BinaryConverterError.streamIsShort: Could not read the value from the stream
-    public class func convert<T: BinaryCompatible>(binary stream: ReadableByteStream, count: Int, byteOrder: ByteOrder? = nil) throws -> [T] {
+    public class func parse<T: BinaryCompatible>(binary stream: ReadableByteStream, count: Int, byteOrder: ByteOrder? = nil) throws -> [T] {
         var value = [] as [T]
         for _ in 0..<count {
             value.append(try T(parsing: stream, byteOrder: byteOrder))
@@ -105,15 +105,15 @@ public class BinaryConverter {
     }
     
     
-    public class func convert<Key: Hashable>(binary array: Array<UInt8>, layout: Array<(Key, BinaryCompatible.Type, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
-        return try convert(binary: ReadableByteStreamRefering(array[0..<array.count]), layout: layout, defaultByteOrder: defaultByteOrder)
+    public class func parse<Key: Hashable>(binary array: Array<UInt8>, layout: Array<(Key, BinaryCompatible.Type, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
+        return try parse(binary: ReadableByteStreamRefering(array[0..<array.count]), layout: layout, defaultByteOrder: defaultByteOrder)
     }
     
-    public class func convert<Key: Hashable>(binary arraySlice: ArraySlice<UInt8>, layout: Array<(Key, BinaryCompatible.Type, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
-        return try convert(binary: ReadableByteStreamRefering(arraySlice), layout: layout, defaultByteOrder: defaultByteOrder)
+    public class func parse<Key: Hashable>(binary arraySlice: ArraySlice<UInt8>, layout: Array<(Key, BinaryCompatible.Type, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
+        return try parse(binary: ReadableByteStreamRefering(arraySlice), layout: layout, defaultByteOrder: defaultByteOrder)
     }
     
-    public class func convert<Key: Hashable>(binary stream: ReadableByteStream, layout: Array<(Key, BinaryCompatible.Type, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
+    public class func parse<Key: Hashable>(binary stream: ReadableByteStream, layout: Array<(Key, BinaryCompatible.Type, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
         var result: [Key : Any] = [:]
         for (key, type, byteOrder) in layout {
             result[key] = try type.init(parsing: stream, byteOrder: byteOrder ?? defaultByteOrder)
@@ -122,15 +122,15 @@ public class BinaryConverter {
     }
 
     
-    public class func convert<Key: Hashable>(binary array: Array<UInt8>, layout: Array<(Key, BinaryType, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
-        return try convert(binary: ReadableByteStreamRefering(array[0..<array.count]), layout: layout, defaultByteOrder: defaultByteOrder)
+    public class func parse<Key: Hashable>(binary array: Array<UInt8>, layout: Array<(Key, BinaryType, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
+        return try parse(binary: ReadableByteStreamRefering(array[0..<array.count]), layout: layout, defaultByteOrder: defaultByteOrder)
     }
     
-    public class func convert<Key: Hashable>(binary arraySlice: ArraySlice<UInt8>, layout: Array<(Key, BinaryType, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
-        return try convert(binary: ReadableByteStreamRefering(arraySlice), layout: layout, defaultByteOrder: defaultByteOrder)
+    public class func parse<Key: Hashable>(binary arraySlice: ArraySlice<UInt8>, layout: Array<(Key, BinaryType, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
+        return try parse(binary: ReadableByteStreamRefering(arraySlice), layout: layout, defaultByteOrder: defaultByteOrder)
     }
     
-    public class func convert<Key: Hashable>(binary stream: ReadableByteStream, layout: Array<(Key, BinaryType, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
+    public class func parse<Key: Hashable>(binary stream: ReadableByteStream, layout: Array<(Key, BinaryType, ByteOrder?)>, defaultByteOrder: ByteOrder? = nil) throws -> Dictionary<Key, Any> {
         var result: [Key : Any] = [:]
         for (key, type, byteOrder) in layout {
             switch type {
@@ -149,13 +149,13 @@ public class BinaryConverter {
         return result
     }
     
-    // MARK: - Converting `BinaryCompatible` value(s) into `[UInt8]`
+    // MARK: - Binarizing `BinaryCompatible` value(s) into `[UInt8]`
     
-    public class func convert(value: BinaryCompatible, byteOrder: ByteOrder? = nil) -> [UInt8] {
+    public class func binarize(value: Binarizable, byteOrder: ByteOrder? = nil) -> [UInt8] {
         return value.binarize(byteOrder: byteOrder)
     }
     
-    public class func convert<T: BinaryCompatible>(values: Array<T>, byteOrder: ByteOrder? = nil) -> [UInt8] {
+    public class func binarize<T: Binarizable>(values: Array<T>, byteOrder: ByteOrder? = nil) -> [UInt8] {
         var binary: [UInt8] = []
         for value in values {
             binary.append(contentsOf: value.binarize(byteOrder: byteOrder))
@@ -164,19 +164,19 @@ public class BinaryConverter {
     }
     
     // FIXME: limit type of `mixedValues` to as like Array<Binarizable>, but `Array` can not inherit protocol with 'Element' constraints in Swift 3.0.x. And I want to remove the `throws`.
-    public class func convert(mixedValues: Array<Any>, byteOrder: ByteOrder? = nil) throws -> [UInt8] {
+    public class func binarize(mixedValues: Array<Any>, byteOrder: ByteOrder? = nil) throws -> [UInt8] {
         var binary: [UInt8] = []
         for unknownTypeValue in mixedValues {
             switch unknownTypeValue {
             case let value as BinaryCompatible:
-                binary.append(contentsOf: convert(value: value, byteOrder: byteOrder))
+                binary.append(contentsOf: binarize(value: value, byteOrder: byteOrder))
             case let values as Array<BinaryCompatible>:
                 #if false
                     // Error: in Swift 3.0.x
-                    array.append(contentsOf: convert(values: values, byteOrder: byteOrder))
+                    array.append(contentsOf: binarize(values: values, byteOrder: byteOrder))
                 #else
                     for value in values {
-                        binary.append(contentsOf: convert(value: value, byteOrder: byteOrder))
+                        binary.append(contentsOf: binarize(value: value, byteOrder: byteOrder))
                     }
                 #endif
             default:
@@ -188,17 +188,6 @@ public class BinaryConverter {
     
 }
 
-//
-// I want to write as following, but it is ill-formed in Swift 3.0.x
-//
-//public protocol Binarizable {
-//    func convertIntoBinary(byteOrder: ByteOrder?) -> [UInt8]
-//}
-//
-//// Error: Extension of protocol 'BinaryCompatible' cannot have an inheritance clause
-//extension BinaryCompatible: Binarizable {
-//}
-//
 //// Error: Extension of type 'Array' with constraints cannot have an inheritance clause
 //extension Array: Binarizable where Element: BinaryCompatible {
 //    internal func convertIntoBinary(byteOrder: ByteOrder?) -> [UInt8] {
--- Tests/BinaryConverterTests.swift
+++ Tests/BinaryConverterTests.swift
@@ -19,12 +19,12 @@ extension SimpleStructForTest: BinaryCompatible {
         let layout: [(String, BinaryCompatible.Type, ByteOrder?)] = [
             ("id", UInt8.self, nil),
             ("count", UInt16.self, nil)]
-        let converted = try BinaryConverter.convert(binary: stream, layout: layout, defaultByteOrder: byteOrder)
+        let converted = try BinaryConverter.parse(binary: stream, layout: layout, defaultByteOrder: byteOrder)
         self = SimpleStructForTest(id: converted["id"] as! UInt8, count: converted["count"] as! UInt16)
     }
     
     public func binarize(byteOrder: ByteOrder?) -> [UInt8] {
-        return try! BinaryConverter.convert(mixedValues: [id, count], byteOrder: byteOrder)
+        return try! BinaryConverter.binarize(mixedValues: [id, count], byteOrder: byteOrder)
     }
 }
 
@@ -35,25 +35,25 @@ class BinaryConverterTests: XCTestCase {
     }
     
     func testConvertingLittleEndianIntoValue() {
-        let result = try! BinaryConverter.convert(binary: [0x80, 0x00], byteOrder: .littleEndian) as Int16
+        let result = try! BinaryConverter.parse(binary: [0x80, 0x00], byteOrder: .littleEndian) as Int16
         XCTAssert(result == 128)
     }
     
     func testConvertingBigEndianIntoValue() {
-        let result = try! BinaryConverter.convert(binary: [0x80, 0x00], byteOrder: .bigEndian) as Int16
+        let result = try! BinaryConverter.parse(binary: [0x80, 0x00], byteOrder: .bigEndian) as Int16
         XCTAssert(result == -32768)
     }
     
     func testConvertingBinaryIntoData() {
         let binary = [0x01, 0x02, 0x03, 0x04, 0xAA, 0x55, 0xFF, 0x00] as [UInt8]
-        let result = try! BinaryConverter.convert(binary: binary) as Data
-        XCTAssert(binary == BinaryConverter.convert(value: result))
+        let result = try! BinaryConverter.parse(binary: binary) as Data
+        XCTAssert(binary == BinaryConverter.binarize(value: result))
     }
     
     func testConvertingFixedArrayIntoValue() {
         let asciiz8 = [0x41, 0x53, 0x43, 0x49, 0x49, 0x00, 0x00, 0x00] as [UInt8]
         XCTAssert(asciiz8.count == 8)
-        var result = try! BinaryConverter.convert(binary: asciiz8, count: asciiz8.count, byteOrder: nil) as [CChar]
+        var result = try! BinaryConverter.parse(binary: asciiz8, count: asciiz8.count, byteOrder: nil) as [CChar]
         XCTAssert(result.count == 8)
         let str = withUnsafePointer(to: &result[0]) {
             String(cString: $0)
@@ -63,7 +63,7 @@ class BinaryConverterTests: XCTestCase {
     
     func testConvertingBinaryIntoValues1() {
         // ui16=.little, ui32=.big
-        let values = try! BinaryConverter.convert(
+        let values = try! BinaryConverter.parse(
             binary: [0xff, 0x7f, 0x01, 0x02, 0x03, 0x04],
             layout: [("ui16", UInt16.self, .littleEndian), ("ui32", UInt32.self, nil)],
             defaultByteOrder: .bigEndian)
@@ -76,7 +76,7 @@ class BinaryConverterTests: XCTestCase {
     func testConvertingBinaryIntoValues2() {
         // id: UInt8, asciiz: [CChar](count: 8)
         let data = Data.init(bytes: [0x01, 0x41, 0x53, 0x43, 0x49, 0x49, 0x00, 0x00, 0x00])
-        let values = try! BinaryConverter.convert(binary: ReadableByteStreamRefering(data), layout: [("id", BinaryType(UInt8.self), nil), ("asciiz", BinaryType(CChar.self, count: 8), nil)])
+        let values = try! BinaryConverter.parse(binary: ReadableByteStreamRefering(data), layout: [("id", BinaryType(UInt8.self), nil), ("asciiz", BinaryType(CChar.self, count: 8), nil)])
         XCTAssert(values["id"] is UInt8)
         XCTAssert(values["id"] as! UInt8 == 0x01)
         XCTAssert(values["asciiz"] is [CChar])
@@ -91,7 +91,7 @@ class BinaryConverterTests: XCTestCase {
     func testConvertingCustomStructIntoValue() {
         let array = [0x10, 0x00, 0x08] as [UInt8]
         let stream = ReadableByteStreamRefering(array[0...2])
-        let values = try! BinaryConverter.convert(binary: stream, byteOrder: .bigEndian) as SimpleStructForTest
+        let values = try! BinaryConverter.parse(binary: stream, byteOrder: .bigEndian) as SimpleStructForTest
         XCTAssert(values.id == 0x10)
         XCTAssert(values.count == 8)
     }
@@ -99,13 +99,13 @@ class BinaryConverterTests: XCTestCase {
     
     func testConvertingValueIntoLittleEndian() {
         let value = 128 as Int16
-        let result = BinaryConverter.convert(value: value, byteOrder: .littleEndian)
+        let result = BinaryConverter.binarize(value: value, byteOrder: .littleEndian)
         XCTAssert(result == [0x80, 0x00])
     }
     
     func testConvertingValuesIntoBigEndian() {
         let values = [0x7fff, 0x0102] as [UInt16]
-        let result = BinaryConverter.convert(values: values, byteOrder: .bigEndian)
+        let result = BinaryConverter.binarize(values: values, byteOrder: .bigEndian)
         XCTAssert(result == [0x7f, 0xff, 0x01, 0x02])
     }
     
@@ -119,7 +119,7 @@ class BinaryConverterTests: XCTestCase {
             0x01020304 as UInt32,
             asciiz8
         ] as [Any]
-        let result = try! BinaryConverter.convert(mixedValues: values, byteOrder: .bigEndian)
+        let result = try! BinaryConverter.binarize(mixedValues: values, byteOrder: .bigEndian)
         XCTAssert(result == [
             0x00, 0x80,
             0x01, 0x02, 0x03, 0x04,
@@ -129,29 +129,29 @@ class BinaryConverterTests: XCTestCase {
     
     func testConvertingCustomStructIntoBinary() {
         let value = SimpleStructForTest(id: 0x10, count: 8)
-        let binary = BinaryConverter.convert(value: value, byteOrder: .bigEndian)
+        let binary = BinaryConverter.binarize(value: value, byteOrder: .bigEndian)
         XCTAssert(binary == [0x10, 0x00, 0x08])
     }
     
     
     func testExample() {
-        print(try! BinaryConverter.convert(binary: [0x80, 0x00], byteOrder: .littleEndian) as Int16) // 128
-        print(try! BinaryConverter.convert(binary: [0x80, 0x00], byteOrder: .bigEndian) as Int16) // -32768
+        print(try! BinaryConverter.parse(binary: [0x80, 0x00], byteOrder: .littleEndian) as Int16) // 128
+        print(try! BinaryConverter.parse(binary: [0x80, 0x00], byteOrder: .bigEndian) as Int16) // -32768
         
-        var asciiz8 = try! BinaryConverter.convert(binary: [0x41, 0x53, 0x43, 0x49, 0x49, 0x00, 0x00, 0x00], count: 8) as [CChar]
+        var asciiz8 = try! BinaryConverter.parse(binary: [0x41, 0x53, 0x43, 0x49, 0x49, 0x00, 0x00, 0x00], count: 8) as [CChar]
         let str = withUnsafePointer(to: &asciiz8[0]) { String(cString: $0) }
         print(str) // "ASCII"
         
         // id: UInt8, asciiz: [CChar](count: 8)
         let array = [0x01, 0x41, 0x53, 0x43, 0x49, 0x49, 0x00, 0x00, 0x00] as [UInt8]
-        let values = try! BinaryConverter.convert(binary: array, layout: [("id", BinaryType(UInt8.self), nil), ("asciiz", BinaryType(CChar.self, count: 8), nil)]) // ["id": 1, "asciiz": [65, 83, 67, 73, 73, 0, 0, 0]]
+        let values = try! BinaryConverter.parse(binary: array, layout: [("id", BinaryType(UInt8.self), nil), ("asciiz", BinaryType(CChar.self, count: 8), nil)]) // ["id": 1, "asciiz": [65, 83, 67, 73, 73, 0, 0, 0]]
         print(values)
         
         
-        print(BinaryConverter.convert(value: 128 as Int16, byteOrder: .littleEndian))
-        print(BinaryConverter.convert(values: [0x7fff, 0x0102] as [Int16], byteOrder: .bigEndian))
+        print(BinaryConverter.binarize(value: 128 as Int16, byteOrder: .littleEndian))
+        print(BinaryConverter.binarize(values: [0x7fff, 0x0102] as [Int16], byteOrder: .bigEndian))
         
-        let result = try! BinaryConverter.convert(mixedValues: [-32768 as Int16, 0x01020304 as UInt32, "ASCII".cString(using: .ascii)!], byteOrder: .bigEndian)
+        let result = try! BinaryConverter.binarize(mixedValues: [-32768 as Int16, 0x01020304 as UInt32, "ASCII".cString(using: .ascii)!], byteOrder: .bigEndian)
         print(result) // [128, 0, 1, 2, 3, 4, 65, 83, 67, 73, 73, 0]
     }
 }
